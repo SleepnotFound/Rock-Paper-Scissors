268a1257affc7d1c63832de7a4c10a2e72f7dd7c
Javi <jav.olivares3@gmail.com>
2022-01-01 15:56:57 -0600

    images now dont stack.cleaned up visual ui.

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const choice = ['rock','paper','scissors'];                                                 //available options for player/computer
+const result = ['tied', 'win', 'lose'];
 let playerPoints = 0;
 let computerPoints = 0;
 let round = 0;
+let imgRock = 'images/RockBubble.PNG';
+let imgPaper = 'images/PaperBubble.PNG';
+let imgScissors = 'images/ScissorsBubble.PNG';
+let imgThumbs = 'images/thumbsUp.PNG';
+let imgPointsRight = 'images/PointsRight.PNG';
+let imgPointsLeft  = 'images/PointsLeft.PNG'
+let imgclap = 'images/Clap.PNG';
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
@@ -25,7 +33,6 @@ function playGame(decision) {
 }
 
 function chooseResult(computerPlay, playerPlay) {
-    let result = ['tie', 'win', 'lose'];
     if (computerPlay == choice[0]) {
         switch (playerPlay) {
             case choice[0]:
@@ -85,13 +92,47 @@ function displayMatch(player, computer, result) {
     const yourPoints = document.querySelector('.yourPoints');
     const comPoints = document.querySelector('.comPoints');
     const currentRound = document.querySelector('.currentRound');
+    const firstImg = document.querySelector('.plaImage img');
+    const secondImg = document.querySelector('.resImage img');
+    const thirdImg = document.querySelector('.comImage img');
+
+    firstImg.src = choosePic(player);
+    thirdImg.src = choosePic(computer);
+    secondImg.src = chooseRes(result);
 
     uPlays.textContent = `${player}`;  
     comPlays.textContent = `${computer}`;
-    results.textContent = `${result}`;
+    results.textContent = `You ${result}`;
     yourPoints.textContent = `${playerPoints}`;
     comPoints.textContent = `${computerPoints}`;
     currentRound.textContent = `${round}`;
+}
+
+function choosePic (played){
+        switch(played) {
+            case choice[0]:
+                return imgRock;
+                break;
+            case choice[1]:
+                return imgPaper;
+                break;
+            case choice[2]:
+                return imgScissors;
+                break;
+        }
+    }
+function chooseRes (played){
+    switch(played) {
+        case result[0]:
+            return imgThumbs;
+            break;
+        case result[1]:
+            return imgPointsLeft;
+            break;
+        case result[2]:
+            return imgPointsRight;
+            break;
+    }
 }
 
 function endGame() {
